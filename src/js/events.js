08620067d4028869408c05f7d4687f081f877be8
Tiago <tiagopereira14200@gmail.com>
2025-02-10 19:13:01 -0300

    Container main em andamento

--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,4 @@
-function scrollEvent() {
+function scrollAsideEvent() {
   const content = document.querySelector(".box_content_aside");
   const thumb = document.querySelector(".custom_thumb");
 
@@ -50,7 +50,7 @@ function scrollEvent() {
   updateThumbPosition();
 }
 
-scrollEvent();
+scrollAsideEvent();
 
 function resizeEvent() {
     const box = document.querySelector("aside");
@@ -82,3 +82,69 @@ function resizeEvent() {
   }  
 
 resizeEvent();
+
+
+function scrollMainEvent() {
+  const content = document.querySelector(".container_content_main");
+  const thumb = document.querySelector(".custom_thumb_main");
+
+  function updateThumbPosition() {
+    const scrollHeight = content.scrollHeight;
+    const containerHeight = content.clientHeight;
+    const scrollTop = content.scrollTop;
+
+    const thumbHeight = Math.max(
+      (containerHeight / scrollHeight) * containerHeight,
+      30
+    );
+    thumb.style.height = `${thumbHeight}px`;
+
+    const maxScrollTop = scrollHeight - containerHeight;
+    const maxThumbTop = containerHeight - thumbHeight;
+    const thumbTop = (scrollTop / maxScrollTop) * maxThumbTop;
+
+    thumb.style.top = `${thumbTop}px`;
+
+    if (scrollTop <= 5) {
+      thumb.style.borderRadius = "0 0.5rem 0 0";
+    } 
+    else if (scrollTop >= maxScrollTop - 5) {
+      thumb.style.borderRadius = "0 0 0.5rem 0";
+    } 
+    else {
+      thumb.style.borderRadius = "0";
+    }
+  }
+
+  content.addEventListener("scroll", updateThumbPosition);
+
+  thumb.addEventListener("mousedown", (e) => {
+    const startY = e.clientY;
+    const startTop = parseFloat(thumb.style.top) || 0;
+
+    function onMouseMove(event) {
+      const deltaY = event.clientY - startY;
+      const maxThumbTop = content.clientHeight - thumb.clientHeight;
+      let newTop = startTop + deltaY;
+      newTop = Math.max(0, Math.min(newTop, maxThumbTop));
+
+      thumb.style.top = `${newTop}px`;
+
+      const scrollHeight = content.scrollHeight - content.clientHeight;
+      content.scrollTop = (newTop / maxThumbTop) * scrollHeight;
+    }
+
+    function onMouseUp() {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.removeEventListener("mouseup", onMouseUp);
+    }
+
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
+  });
+
+  updateThumbPosition();
+}
+
+
+scrollMainEvent();
